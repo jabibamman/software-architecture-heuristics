5cc78290fa1b256eff7a983edc045c81410d7534
Ronan Kielt <kakapo9156@gmail.com>
2025-05-27 17:10:02 +0200

    feat: scaffold back architecture

--- apps/parking-app-back/src/app.module.ts
+++ apps/parking-app-back/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ReservationsModule } from './reservations/reservations.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { StatsModule } from './stats/stats.module';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
-  imports: [],
+  imports: [ReservationsModule, UsersModule, AuthModule, StatsModule, NotificationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
