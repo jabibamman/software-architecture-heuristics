2205790dcf99b7db391c19766a50f2b5d212e85f
Ronan Kielt <kakapo9156@gmail.com>
2025-05-27 23:03:45 +0200

    feat: stats use case and controller stubs

--- apps/parking-app-back/src/stats/interface/http/stats.controller.ts
+++ apps/parking-app-back/src/stats/interface/http/stats.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { GetParkingStatsUseCase } from '../../application/use-cases/get-parking-stats.use-case';
 
 @Controller('stats')
-export class StatsController {}
+export class StatsController {
+  constructor(
+    private readonly getParkingStatsUseCase: GetParkingStatsUseCase,
+  ) {}
+
+  @Get()
+  getParkingStats(): Promise<void> {
+    return this.getParkingStatsUseCase.execute();
+  }
+}
--- apps/parking-app-back/src/stats/stats.module.ts
+++ apps/parking-app-back/src/stats/stats.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { StatsController } from './interface/http/stats.controller';
+import { GetParkingStatsUseCase } from './application/use-cases/get-parking-stats.use-case';
 
-@Module({})
+@Module({
+  imports: [],
+  controllers: [StatsController],
+  providers: [GetParkingStatsUseCase],
+  exports: [],
+})
 export class StatsModule {}
