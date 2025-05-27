153569af4a7417ea845261951d71e7eb42c20d4c
Ronan Kielt <kakapo9156@gmail.com>
2025-05-27 17:15:49 +0200

    feat: add health check controller

--- apps/parking-app-back/src/app.module.ts
+++ apps/parking-app-back/src/app.module.ts
@@ -1,15 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReservationsModule } from './reservations/reservations.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { StatsModule } from './stats/stats.module';
 import { NotificationsModule } from './notifications/notifications.module';
+import { HealthController } from './health.controller';
 
 @Module({
-  imports: [ReservationsModule, UsersModule, AuthModule, StatsModule, NotificationsModule],
-  controllers: [AppController],
+  imports: [
+    ReservationsModule,
+    UsersModule,
+    AuthModule,
+    StatsModule,
+    NotificationsModule,
+  ],
+  controllers: [HealthController],
   providers: [AppService],
 })
 export class AppModule {}
