dd45b329e4af50bab23a3591e5494ecf21a5187b
cretois charles <charlescretois@gmail.com>
2025-05-30 17:15:57 +0200

    feat: homepage fix

--- apps/parking-app-front/src/components/TheWelcome.vue
+++ apps/parking-app-front/src/components/TheWelcome.vue
@@ -10,85 +10,123 @@ const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
 </script>
 
 <template>
-  <WelcomeItem>
-    <template #icon>
-      <DocumentationIcon />
-    </template>
-    <template #heading>Documentation</template>
+  <div class="min-h-screen py-12 px-6 bg-gradient-to-br from-slate-50 via-white to-slate-100">
+    <div class="max-w-5xl mx-auto text-center mb-12">
+      <h1 class="text-4xl sm:text-5xl font-extrabold text-indigo-700 mb-4">
+        🎉 Bienvenue sur votre projet Vue 3
+      </h1>
+      <p class="text-lg text-gray-600">
+        Voici quelques liens utiles pour bien démarrer et explorer l'écosystème Vue.
+      </p>
+    </div>
 
-    Vue’s
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
-    provides you with all information you need to get started.
-  </WelcomeItem>
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 max-w-6xl mx-auto">
+      <WelcomeItem>
+        <template #icon><DocumentationIcon /></template>
+        <template #heading>Documentation</template>
+        Vue’s
+        <a
+          href="https://vuejs.org/"
+          target="_blank"
+          rel="noopener"
+          class="text-indigo-600 underline hover:text-indigo-800 transition"
+        >
+          official documentation
+        </a>
+        provides all the information you need to get started.
+      </WelcomeItem>
 
-  <WelcomeItem>
-    <template #icon>
-      <ToolingIcon />
-    </template>
-    <template #heading>Tooling</template>
+      <WelcomeItem>
+        <template #icon><ToolingIcon /></template>
+        <template #heading>Tooling</template>
+        Bundled with
+        <a href="https://vitejs.dev" class="text-green-600 underline hover:text-green-800">Vite</a>,
+        recommended IDE:
+        <a
+          href="https://code.visualstudio.com/"
+          class="text-green-600 underline hover:text-green-800"
+          >VSCode</a
+        >
+        +
+        <a
+          href="https://github.com/vuejs/language-tools"
+          class="text-green-600 underline hover:text-green-800"
+          >Vue Tools</a
+        >. Test with
+        <a href="https://vitest.dev/" class="text-green-600 underline hover:text-green-800"
+          >Vitest</a
+        >,
+        <a href="https://www.cypress.io/" class="text-green-600 underline hover:text-green-800"
+          >Cypress</a
+        >
+        or
+        <a href="https://playwright.dev/" class="text-green-600 underline hover:text-green-800"
+          >Playwright</a
+        >.
+        <br />
+        <a
+          href="javascript:void(0)"
+          @click="openReadmeInEditor"
+          class="text-sm text-gray-500 underline hover:text-gray-700 mt-2 inline-block"
+        >
+          Voir le README.md
+        </a>
+      </WelcomeItem>
 
-    This project is served and bundled with
-    <a href="https://vite.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The
-    recommended IDE setup is
-    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a>
-    +
-    <a href="https://github.com/vuejs/language-tools" target="_blank" rel="noopener">Vue - Official</a>. If
-    you need to test your components and web pages, check out
-    <a href="https://vitest.dev/" target="_blank" rel="noopener">Vitest</a>
-    and
-    <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a>
-    /
-    <a href="https://playwright.dev/" target="_blank" rel="noopener">Playwright</a>.
+      <WelcomeItem>
+        <template #icon><EcosystemIcon /></template>
+        <template #heading>Ecosystem</template>
+        Explorez :
+        <a href="https://pinia.vuejs.org/" class="text-orange-600 underline hover:text-orange-800"
+          >Pinia</a
+        >,
+        <a href="https://router.vuejs.org/" class="text-orange-600 underline hover:text-orange-800"
+          >Vue Router</a
+        >,
+        <a
+          href="https://test-utils.vuejs.org/"
+          class="text-orange-600 underline hover:text-orange-800"
+          >Vue Test Utils</a
+        >,
+        <a
+          href="https://github.com/vuejs/devtools"
+          class="text-orange-600 underline hover:text-orange-800"
+          >Devtools</a
+        >. Visitez
+        <a
+          href="https://github.com/vuejs/awesome-vue"
+          class="text-orange-600 underline hover:text-orange-800"
+          >Awesome Vue</a
+        >
+        pour plus de ressources.
+      </WelcomeItem>
 
-    <br />
+      <WelcomeItem>
+        <template #icon><CommunityIcon /></template>
+        <template #heading>Communauté</template>
+        Posez vos questions sur
+        <a href="https://chat.vuejs.org" class="text-pink-600 underline hover:text-pink-800"
+          >Vue Land</a
+        >,
+        <a
+          href="https://stackoverflow.com/questions/tagged/vue.js"
+          class="text-pink-600 underline hover:text-pink-800"
+          >StackOverflow</a
+        >. Suivez
+        <a href="https://x.com/vuejs" class="text-pink-600 underline hover:text-pink-800">@vuejs</a>
+        pour rester à jour.
+      </WelcomeItem>
 
-    More instructions are available in
-    <a href="javascript:void(0)" @click="openReadmeInEditor"><code>README.md</code></a
-    >.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <EcosystemIcon />
-    </template>
-    <template #heading>Ecosystem</template>
-
-    Get official tools and libraries for your project:
-    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
-    <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>,
-    <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener">Vue Test Utils</a>, and
-    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>. If
-    you need more resources, we suggest paying
-    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
-    a visit.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <CommunityIcon />
-    </template>
-    <template #heading>Community</template>
-
-    Got stuck? Ask your question on
-    <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>
-    (our official Discord server), or
-    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
-      >StackOverflow</a
-    >. You should also follow the official
-    <a href="https://bsky.app/profile/vuejs.org" target="_blank" rel="noopener">@vuejs.org</a>
-    Bluesky account or the
-    <a href="https://x.com/vuejs" target="_blank" rel="noopener">@vuejs</a>
-    X account for latest news in the Vue world.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <SupportIcon />
-    </template>
-    <template #heading>Support Vue</template>
-
-    As an independent project, Vue relies on community backing for its sustainability. You can help
-    us by
-    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
-  </WelcomeItem>
+      <WelcomeItem>
+        <template #icon><SupportIcon /></template>
+        <template #heading>Soutenir Vue</template>
+        Aidez Vue à rester indépendant en
+        <a href="https://vuejs.org/sponsor/" class="text-yellow-600 underline hover:text-yellow-800"
+          >devenant sponsor</a
+        >.
+        <br />
+        Merci pour votre soutien 🙏
+      </WelcomeItem>
+    </div>
+  </div>
 </template>
