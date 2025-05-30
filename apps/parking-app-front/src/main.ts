91ba802b1bd1d50e4257d1c7ed311793b31eb451
vithushan77 <vithushanarul08@gmail.com>
2025-05-30 18:09:23 +0200

    conct to back

--- apps/parking-app-front/src/main.ts
+++ apps/parking-app-front/src/main.ts
@@ -1,14 +1,17 @@
+// src/main.ts
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
+import {useAuthStore} from "@/stores/useAuthStore.ts";
 
 const app = createApp(App)
+const pinia = createPinia()
+app.use(pinia)
 
-app.use(createPinia())
+const auth = useAuthStore()
+auth.initialize()
+
 app.use(router)
-
 app.mount('#app')
