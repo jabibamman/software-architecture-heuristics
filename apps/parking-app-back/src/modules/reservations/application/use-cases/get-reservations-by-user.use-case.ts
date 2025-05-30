41f27002356b0306a4bda31ce22a2e59b8a2602a
Ronan Kielt <kakapo9156@gmail.com>
2025-05-30 23:36:42 +0200

    feat: get reservations by user id use case

--- apps/parking-app-back/src/modules/reservations/application/use-cases/get-reservations-by-user.use-case.ts
+++ apps/parking-app-back/src/modules/reservations/application/use-cases/get-reservations-by-user.use-case.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable, NotImplementedException } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { ReservationRepositoryPort } from '@/modules/reservations/application/ports/reservation.repository.port';
 import { ReservationResponseDto } from '@/modules/reservations/application/dtos';
 import { JwtPayload } from '@/modules/auth/application/dtos';
@@ -11,6 +11,7 @@ export class GetReservationsByUserUseCase {
   ) {}
 
   async execute(payload: JwtPayload): Promise<ReservationResponseDto[]> {
-    throw new NotImplementedException();
+    const reservations = await this.repo.findByUserId(payload.userId);
+    return reservations.map((r) => ReservationResponseDto.fromEntity(r));
   }
 }
--- apps/parking-app-back/src/modules/reservations/interface/http/reservation.controller.ts
+++ apps/parking-app-back/src/modules/reservations/interface/http/reservation.controller.ts
@@ -2,18 +2,19 @@ import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import {
   ReservationCreationDto,
   ReservationResponseDto,
+  CheckInReservationDto,
 } from '../../application/dtos';
 import {
   CheckInReservationUseCase,
   CreateReservationUseCase,
   GetReservationUseCase,
+  GetReservationsByUserUseCase,
   GetReservationsUseCase,
 } from '../../application/use-cases';
-import { CheckInReservationDto } from '../../application/dtos/checkin-reservation.dto';
 import { JwtAuthGuard } from '@/common/guards/jwt-auth.guard';
 import { ApiBearerAuth } from '@nestjs/swagger';
 import { CurrentUser } from '@/common/decorators/current-user.decorator';
-import { JwtPayload } from '@/modules/auth/application/dtos/jwt-payload';
+import { JwtPayload } from '@/modules/auth/application/dtos';
 
 @Controller('reservation')
 export class ReservationController {
@@ -21,7 +22,7 @@ export class ReservationController {
     private readonly createReservationUseCase: CreateReservationUseCase,
     private readonly getReservationUseCase: GetReservationUseCase,
     private readonly getReservationsUseCase: GetReservationsUseCase,
-
+    private readonly getReservationsByUserUseCase: GetReservationsByUserUseCase,
     private readonly checkInReservationUseCase: CheckInReservationUseCase,
   ) {}
 
@@ -42,11 +43,20 @@ export class ReservationController {
     return this.getReservationUseCase.execute(id);
   }
 
+  @Get('all')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  getAllReservations(): Promise<ReservationResponseDto[]> {
+    return this.getReservationsUseCase.execute();
+  }
+
   @Get()
   @UseGuards(JwtAuthGuard)
   @ApiBearerAuth()
-  getReservations(): Promise<ReservationResponseDto[]> {
-    return this.getReservationsUseCase.execute();
+  getReservationsForCurrentUser(
+    @CurrentUser() payload: JwtPayload,
+  ): Promise<ReservationResponseDto[]> {
+    return this.getReservationsByUserUseCase.execute(payload);
   }
 
   @Post(':id/checkin')
